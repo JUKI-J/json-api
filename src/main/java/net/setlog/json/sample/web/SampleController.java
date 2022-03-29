4dfbe91f9201b81d3a3f75004d5bd418ed687eb8
JUKI-J <jju.ki@hotmail.com>
2022-03-29 12:39:47 +0900

    + delete log file contents

--- src/main/java/net/setlog/json/sample/web/SampleController.java
+++ src/main/java/net/setlog/json/sample/web/SampleController.java
@@ -1,9 +1,15 @@
 package net.setlog.json.sample.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -20,6 +26,12 @@ public class SampleController {
         map.put("requestURI", request.getRequestURI());
         map.put("request-method", request.getMethod());
         return map;
+    }
+
+    @RequestMapping(value = "/empty", method={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+    public ResponseEntity emptyLogFile(HttpServletRequest request) throws IOException {
+        FileChannel.open(Paths.get("./logs/json-api.log"), StandardOpenOption.WRITE).truncate(0).close();
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
